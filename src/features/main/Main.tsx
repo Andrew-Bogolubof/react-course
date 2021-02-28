9d84f56fcfdd63558f10afd5794025e77443e211
Andrei Ahanouski <andrei_ahanouski@epam.com>
2021-02-28 18:50:06 +0300

    feat: decompose components

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -5,6 +5,8 @@ import { MovieCard } from '../movie-card';
 
 // TODO: remove mock movies and genres
 import moviesList from '../../mocks/movies.json';
+import { DropDown } from '../common/toggle';
+import { SortLineList } from '../common/sort-line-list';
 
 const genres = Array.from(
   moviesList
@@ -19,36 +21,18 @@ export interface MainProps {}
 
 const Main: React.FunctionComponent<MainProps> = () => {
   const [movies] = useState(moviesList.slice(1, 7));
-  const [selectedGenre, setSelectedGenre] = useState(genres[0]);
   return (
     <main className={`container-fluid ${classes.main}`}>
       <ErrorBoundary>
         <div className="container-xl">
           <div className={`row d-flex align-items-center ${classes.movies_sort}`}>
             <div className={`col-sm p-3 d-flex ${classes.sort_row}`}>
-              {genres.map((genre) => (
-                <div key={genre} className={`text-center mr-2 ${classes.genre}`}>
-                  {genre}
-                  <div className={`${genre === selectedGenre ? classes.genre_selected : ''}`} />
-                </div>
-              ))}
+              <SortLineList list={genres} />
             </div>
             <div className="col-sm d-flex justify-content-end align-items-center">
               <div className="sm text-muted">Sort By</div>
               <div className="col-smd">
-                <div className="dropdown">
-                  <button
-                    className={`btn dropdown-toggle ${classes.dropdown_button}`}
-                    type="button"
-                    id="dropdownMenuButton"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Option
-                  </button>
-                  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton" />
-                </div>
+                <DropDown options={['someOption']} />
               </div>
             </div>
           </div>
